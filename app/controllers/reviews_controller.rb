f33b28ebc11e2907955286f7ea9dd6e53be83294
Thomas Goulet <t.gouletpro@gmail.com>
2017-03-08 17:54:38 +0100

    Exo 01 v2

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,22 @@
 class ReviewsController < ApplicationController
-  def index
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
   end
 
-  def new
-  end
+  private
 
-  def edit
-  end
-
-  def show
-  end
-
-  def update
-  end
-
-  def destroy
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
